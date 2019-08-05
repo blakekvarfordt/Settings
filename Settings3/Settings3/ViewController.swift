6d5345749e9552645ea8093c865e62d58c2b4d43
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-05 10:07:32 -0600

    initial commit

---
+++ Settings3/Settings3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settings3
+//
+//  Created by Blake kvarfordt on 8/5/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
