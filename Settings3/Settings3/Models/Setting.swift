3b713b98c6925ba6f1b4dbbbcd68ae6606270502
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-05 10:29:12 -0600

    Sets up settings model

---
+++ Settings3/Settings3/Models/Setting.swift
@@ -0,0 +1,23 @@
+//
+//  Setting.swift
+//  Settings3
+//
+//  Created by Blake kvarfordt on 8/5/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class Setting {
+    
+    let name: String
+    let icon: UIImage
+    let isOn: Bool
+    
+    init(name: String, icon: UIImage, isOn: Bool) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+    
+}
